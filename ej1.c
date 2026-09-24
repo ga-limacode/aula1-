a3723fb808e1ca9f8df0ebe75e4d3d3f19c38bdb
ga-limacode <gabrielfranciscolima2007@gmail.com>
2026-09-24 20:22:43 -0300

    Update ej1.c

--- ej1.c
+++ ej1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int main () {
+    char nome [50];
+    int idade;
+    float altura;
+
+    printf("nome:");
+    scanf("%s", &nome);
+
+    printf("idade:");
+    scanf("%d", &idade);
+
+    printf("altura:");
+    scanf("%f", &altura);
+
+return 0;
 
