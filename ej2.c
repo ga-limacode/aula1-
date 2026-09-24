fab33c4a87dda1de14a718f4b67a56d519b5f301
ga-limacode <gabrielfranciscolima2007@gmail.com>
2026-09-24 20:59:54 -0300

    ex ej2.c

---
+++ ej2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n1, n2, soma, sub, mult;
+    
+    
+    
+
+    printf("Digite o primeiro número:");
+    scanf("%d", &n1);
+
+    printf("Digite o segundo número:");
+    scanf("%d", &n2);
+
+    soma = n1+n2;
+    sub = n1-n2;
+    mult = n1*n2;
+
+    printf("O valor da soma é: %d", soma);
+    
+    printf("\nO valor da subtração é: %d", sub);
+    
+    printf("\nO valor da multiplicação é: %d", mult );
+
+    
+
+    
+
+    
+
+    
+
+        
+   
+    return 0;
+}
