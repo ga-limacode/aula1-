6e48a7c66f3b1b50a0c3265c030abcbc91b321d7
ga-limacode <gabrielfranciscolima2007@gmail.com>
2026-09-24 21:25:12 -0300

    ej5.c

---
+++ ej5.c
@@ -0,0 +1,18 @@
+ printf("Frequência: ");
+    scanf("%f", &freq);
+
+    soma = n1+n2;
+    media = soma/2;
+
+    printf("A média é %.1f", media);
+
+    if(media<=6, freq<=50){
+        printf("\nAluno Reprovado");
+            
+    }else
+        printf("\nAluno Aprovado");
+    
+    
+    
+    return 0;
+}
